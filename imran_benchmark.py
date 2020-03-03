af6e17601ca050a8b8f44bf7aaf56f782d2ee131
Imran Matin <imranmatin23@gmail.com>
2020-03-03 14:27:20 -0800

    Updated benchmark script to train based on category.

--- imran_benchmark.py
+++ imran_benchmark.py
@@ -3,43 +3,100 @@ import random
 import subprocess 
 import shutil 
 
-CLOSE_IMG_DIR = 'data/gate/label_categories/close/'
-MEDIUM_IMG_DIR = 'data/gate/label_categories/medium/'
-FAR_IMG_DIR = 'data/gate/label_categories/far/'
-EPOCHS = 50
 
-# get the image numbers based on group and shuffle within group
-close_imgs = [img.split('.')[0] for img in os.listdir(CLOSE_IMG_DIR)]
-medium_imgs = [img.split('.')[0] for img in os.listdir(MEDIUM_IMG_DIR)]
-far_imgs = [img.split('.')[0] for img in os.listdir(FAR_IMG_DIR)]
-random.shuffle(close_imgs)
-random.shuffle(medium_imgs)
-random.shuffle(far_imgs)
-
-#get random subset of images of size num_imgs, split 80/20 train/valid
-train_size = int(num_imgs * 0.8)
-train_img = cur_subset[:train_size]
-valid_img = cur_subset[train_size:num_imgs]
-
-#write training and valid images into txt files
-print(f'number of training images: {len(train_img)}, number of valid images: {len(valid_img)}')
-print('Writing train.txt and valid.txt')
-with open('data/gate/train.txt','w') as f:
-    for img in train_img:
-        f.write('data/gate/images/' + img + ".jpg\n")
-
-with open('data/gate/valid.txt','w') as f:
-    for img in valid_img:
-        f.write('data/gate/images/' + img + ".jpg\n")
+########## Use current model state to train on close imgs ###########
 
 #delete contents of checkpoints before training
 shutil.rmtree('checkpoints/')
 os.mkdir('checkpoints/')
 
+# Path to .txt files for close category
+CLOSE_IMG_DIR = 'data/gate/label_categories/close/'
+# Number of epochs to train model on close images
+CLOSE_EPOCHS = 2
+
+# get list of all img numbers in close category and shuffle
+close_imgs = [img.split('.')[0] for img in os.listdir(CLOSE_IMG_DIR)]
+random.shuffle(close_imgs)
+
+#write training and valid images into txt files
+print(f'number of training images: {len(close_imgs)}, number of valid images: {len(close_imgs)}')
+print('Writing train.txt and valid.txt')
+with open('data/gate/train.txt','w') as f:
+    for img in close_imgs:
+        f.write('data/gate/images/' + img + ".jpg\n")
+
+with open('data/gate/valid.txt','w') as f:
+    for img in close_imgs:
+        f.write('data/gate/images/' + img + ".jpg\n")
+
+#train model
+subprocess.run(['python3','-W','ignore','train.py','--model_def','config/yolov3-gate.cfg',
+'--data_config','config/gate.data','--epochs', f'{CLOSE_EPOCHS}','--batch_size','4'])
+
+
+
+
+########## Use current model state to train on medium imgs ###########
+
+# Path to .txt files for medium category
+MEDIUM_IMG_DIR = 'data/gate/label_categories/medium/'
+# Number of epochs to train model on medium images
+MEDIUM_EPOCHS = 2
+
+# get list of all img numbers in medium category and shuffle
+medium_imgs = [img.split('.')[0] for img in os.listdir(MEDIUM_IMG_DIR)]
+random.shuffle(medium_imgs)
+
+#write training and valid images into txt files
+print(f'number of training images: {len(medium_imgs)}, number of valid images: {len(medium_imgs)}')
+print('Writing train.txt and valid.txt')
+with open('data/gate/train.txt','w') as f:
+    for img in medium_imgs:
+        f.write('data/gate/images/' + img + ".jpg\n")
+
+with open('data/gate/valid.txt','w') as f:
+    for img in medium_imgs:
+        f.write('data/gate/images/' + img + ".jpg\n")
+
+#train model
+subprocess.run(['python3','-W','ignore','train.py','--model_def','config/yolov3-gate.cfg',
+'--data_config','config/gate.data','--epochs', f'{MEDIUM_EPOCHS}','--batch_size','4', '--pretrained_weights', f'yolov3_ckpt_{CLOSE_EPOCHS-1}'])
+
+
+
+
+
+# ########## Use current model state to train on far imgs ###########
+
+# # Path to .txt files for far category
+# FAR_IMG_DIR = 'data/gate/label_categories/far/'
+# # Number of epochs to train model on far images
+# FAR_EPOCHS = 10
+
+# # get list of all img numbers in far category and shuffle
+# far_imgs = [img.split('.')[0] for img in os.listdir(FAR_IMG_DIR)]
+# random.shuffle(far_imgs)
+
+# #write training and valid images into txt files
+# print(f'number of training images: {len(far_imgs)}, number of valid images: {len(far_imgs)}')
+# print('Writing train.txt and valid.txt')
+# with open('data/gate/train.txt','w') as f:
+#     for img in far_imgs:
+#         f.write('data/gate/images/' + img + ".jpg\n")
+
+# with open('data/gate/valid.txt','w') as f:
+#     for img in far_imgs:
+#         f.write('data/gate/images/' + img + ".jpg\n")
 
 # #train model
 # subprocess.run(['python3','-W','ignore','train.py','--model_def','config/yolov3-gate.cfg',
-# '--data_config','config/gate.data','--epochs', f'{EPOCHS}','--batch_size','4'])
+# '--data_config','config/gate.data','--epochs', f'{FAR_EPOCHS}','--batch_size','4', '--pretrained_weights', f'yolov3_ckpt_{MEDIUM_EPOCHS-1}'])
+
+
+
+
+# ################ TEST THE MODEL #####################
 
 # #test model for each epoch, record highest mAP and write to file. 
 # metrics = []
