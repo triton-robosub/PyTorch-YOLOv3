39f020fa62badaeb74e279d03ec56ab9343c1a24
Imran Matin <imranmatin23@gmail.com>
2020-03-03 00:36:15 -0800

    Sorted data, both .jpg and .txt, into data folder. Began altering benchmark script.

--- imran_benchmark.py
+++ imran_benchmark.py
@@ -3,63 +3,60 @@ import random
 import subprocess 
 import shutil 
 
-MAX_IMGS = 1900 #upperbound for number of total training labels, currently 1812
-START_IMGS = 700 #anything less will result in errors during validation stage.
-STEP_SIZE = 100
-IMG_DIR = 'data/gate/labels/'
+CLOSE_IMG_DIR = 'data/gate/label_categories/close/'
+MEDIUM_IMG_DIR = 'data/gate/label_categories/medium/'
+FAR_IMG_DIR = 'data/gate/label_categories/far/'
 EPOCHS = 50
 
-all_imgs = os.listdir(IMG_DIR)
-all_imgs = [img.split('.')[0] for img in all_imgs]
-random.shuffle(all_imgs)
+# get the image numbers based on group and shuffle within group
+close_imgs = [img.split('.')[0] for img in os.listdir(CLOSE_IMG_DIR)]
+medium_imgs = [img.split('.')[0] for img in os.listdir(MEDIUM_IMG_DIR)]
+far_imgs = [img.split('.')[0] for img in os.listdir(FAR_IMG_DIR)]
+random.shuffle(close_imgs)
+random.shuffle(medium_imgs)
+random.shuffle(far_imgs)
 
-for num_imgs in range(START_IMGS, MAX_IMGS, STEP_SIZE):
-    print(num_imgs)
+#get random subset of images of size num_imgs, split 80/20 train/valid
+train_size = int(num_imgs * 0.8)
+train_img = cur_subset[:train_size]
+valid_img = cur_subset[train_size:num_imgs]
 
-    #always want to include same images as previous num_imgs
-    #ex. 800 images has same 700 + 100 additional random images, for consistency
-    cur_subset = all_imgs[:num_imgs]
-    random.shuffle(cur_subset)
-    #get random subset of images of size num_imgs, split 80/20 train/valid
-    train_size = int(num_imgs * 0.8)
-    train_img = cur_subset[:train_size]
-    valid_img = cur_subset[train_size:num_imgs]
+#write training and valid images into txt files
+print(f'number of training images: {len(train_img)}, number of valid images: {len(valid_img)}')
+print('Writing train.txt and valid.txt')
+with open('data/gate/train.txt','w') as f:
+    for img in train_img:
+        f.write('data/gate/images/' + img + ".jpg\n")
 
-    #write training and valid images into txt files
-    print(f'number of training images: {len(train_img)}, number of valid images: {len(valid_img)}')
-    print('Writing train.txt and valid.txt')
-    with open('data/gate/train.txt','w') as f:
-        for img in train_img:
-            f.write('data/gate/images/' + img + ".jpg\n")
+with open('data/gate/valid.txt','w') as f:
+    for img in valid_img:
+        f.write('data/gate/images/' + img + ".jpg\n")
 
-    with open('data/gate/valid.txt','w') as f:
-        for img in valid_img:
-            f.write('data/gate/images/' + img + ".jpg\n")
-    
-    #delete contents of checkpoints before training
-    shutil.rmtree('checkpoints/')
-    os.mkdir('checkpoints/')
+#delete contents of checkpoints before training
+shutil.rmtree('checkpoints/')
+os.mkdir('checkpoints/')
 
-    #train model
-    subprocess.run(['python3','-W','ignore','train.py','--model_def','config/yolov3-gate.cfg',
-    '--data_config','config/gate.data','--epochs', f'{EPOCHS}','--batch_size','4'])
 
-    #test model for each epoch, record highest mAP and write to file. 
-    metrics = []
+# #train model
+# subprocess.run(['python3','-W','ignore','train.py','--model_def','config/yolov3-gate.cfg',
+# '--data_config','config/gate.data','--epochs', f'{EPOCHS}','--batch_size','4'])
 
-    for i in range(0, EPOCHS + 1, 5):
-        if i == EPOCHS:
-            i = EPOCHS - 1
-        weights_path = f'checkpoints/yolov3_ckpt_{i}.pth'
+# #test model for each epoch, record highest mAP and write to file. 
+# metrics = []
 
-        subprocess.run(['python3', 'test.py', '--batch_size', '4', '--model_def', 'config/yolov3-gate.cfg', '--data_config', 
-        'config/new_gate.data', '--weights_path', weights_path, '--class_path', 'data/gate/classes.names'])
+# for i in range(0, EPOCHS + 1, 5):
+#     if i == EPOCHS:
+#         i = EPOCHS - 1
+#     weights_path = f'checkpoints/yolov3_ckpt_{i}.pth'
 
-        with open('benchmark_map.txt','r+') as f:
-            mAP = f.read()
-        metrics.append(mAP)
+#     subprocess.run(['python3', 'test.py', '--batch_size', '4', '--model_def', 'config/yolov3-gate.cfg', '--data_config', 
+#     'config/new_gate.data', '--weights_path', weights_path, '--class_path', 'data/gate/classes.names'])
 
-    max_mAP = max(metrics)
-    results = open('benchmark_results.txt', 'a+')
-    results.write(str(num_imgs) + 'images :' + str(max_mAP) + '\n')
-    results.close()
+#     with open('benchmark_map.txt','r+') as f:
+#         mAP = f.read()
+#     metrics.append(mAP)
+
+# max_mAP = max(metrics)
+# results = open('benchmark_results.txt', 'a+')
+# results.write(str(num_imgs) + 'images :' + str(max_mAP) + '\n')
+# results.close()
